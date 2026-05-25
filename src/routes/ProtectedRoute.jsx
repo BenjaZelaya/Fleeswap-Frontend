2f3a4ddd85f6a004c560ccb7f0c858299b1b99ff
LautaroLeall <lautaroleal4@gmail.com>
2026-05-25 20:12:05 -0300

    protectedRote.jsx (protegiendo por si user != admin)

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,17 @@
 import { Navigate, useLocation } from 'react-router-dom'
 import useAuthStore from '../store/authStore'
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute({ children, requireAdmin = false }) {
   const token = useAuthStore((s) => s.token)
+  const user = useAuthStore((s) => s.user)
   const location = useLocation()
 
   if (!token) {
     return <Navigate to="/login" state={{ from: location }} replace />
+  }
+
+  if (requireAdmin && user?.role !== 'ADMIN_ROLE') {
+    return <Navigate to="/" replace />
   }
 
   return children
